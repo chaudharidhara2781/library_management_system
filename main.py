e97b78b131eb49868af389776b42c745fde944e5
chaudharidhara2781 <chaudharidhara2781@gmail.com>
2024-09-01 15:26:14 +0530

    added comment lines in production code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-#-------------------------------------------Test Cases------------------------------------------------------------------
+# -------------------------------------------Test Cases------------------------------------------------------------------
 import unittest
+
 
 class TestLibrary(unittest.TestCase):
 
@@ -54,10 +55,11 @@ class TestLibrary(unittest.TestCase):
         self.assertFalse(self.library.return_book(27))
 
 
-
-#-------------------------------------- Production Code --------------------------------------------------------------------------
+# -------------------------------------- Production Code --------------------------------------------------------------------------
 
 class Book:
+
+    # initializing the book object
     def __init__(self, id, title, author, publication_year):
         self.id = id
         self.title = title
@@ -66,37 +68,48 @@ class Book:
         self.available = True
 
     def __repr__(self):
+        # readable string representation of book obj
         return f"Book({self.id}, '{self.title}', '{self.author}', {self.publication_year}, Available: {self.available})"
 
+
 class Library:
+
+    # initializing library obj with empty list of book
     def __init__(self):
         self.books = []
 
+    # testcase for add_book feature
     def add_book(self, book):
-        if any(existing_book.id == book.id for existing_book in self.books):
+
+        # checking for duplicate book
+        if any(existing_book.id == book.id for existing_book in
+               self.books):  # not adding book if the same book does not exist
             print("Book already exists in the library.")
         else:
             self.books.append(book)
-            print(f"Book '{book.title}' added to the library.")
+            print(f"Book '{book.title}' added to the library.")  # adding book if no same book added
 
+    # testcase for borrowing a book
     def borrow_book(self, book_id):
         for book in self.books:
+            # borrowing a book and marking it as not available
             if book.id == book_id and book.available:
-                book.available = False
+                book.available = False # marked as not available
                 print(f"Book '{book.title}' has been borrowed.")
                 return True
         print("Book not available or not found.")
         return False
 
     def return_book(self, book_id):
+        # returning a borrowed book to library and setting it as available
         for book in self.books:
             if book.id == book_id:
-                if not book.available:
-                    book.available = True
-                    print(f"Book '{book.title}' has been returned.")
+                if not book.available:  # Check if the book was borrowed
+                    book.available = True # marked as available/ returned
+                    print(f"Book '{book.title}' has been returned.") 
                     return True
                 else:
-                    print(f"Book '{book.title}' was not borrowed.")
+                    print(f"Book '{book.title}' was not borrowed.") # print if user tried to return a book that was not borrowed
                     return False
         print("Book not found in the library.")
         return False
